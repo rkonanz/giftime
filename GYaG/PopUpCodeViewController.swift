8dbe871a274b19efe64c2555b48da1e3dac39c1b
rxkonanz <31112801+rxkonanz@users.noreply.github.com>
2018-03-25 15:20:39 -0500

    Now loads image from URL

--- GYaG/PopUpCodeViewController.swift
+++ GYaG/PopUpCodeViewController.swift
@@ -9,13 +9,54 @@
 import UIKit
 
 class PopUpCodeViewController: UIViewController {
-
+    
+    let URL_IMAGE = URL(string: "http://www.wegotyouagift.com/images/pizza.png")
+    
+    @IBOutlet weak var giftImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.layer.cornerRadius = 5;
-        view.layer.masksToBounds = true;
-
+        
+        let session = URLSession(configuration: .default)
+        
+        // create a dataTask
+        let getImageFromUrl = session.dataTask(with: URL_IMAGE!) { (data, response, error) in
+            
+            // check if there is any error
+            if let e = error {
+                
+                // display error message
+                print("Error occurred: \(e)")
+                
+            } else {
+                
+                // in case of no error, check whether response is nil or not
+                if (response as? HTTPURLResponse) != nil {
+                    
+                    // checking if the response contains an image
+                    if let imageData = data {
+                        
+                        // getting the image
+                        let image = UIImage(data: imageData)
+                        
+                        // displaying the image
+                        DispatchQueue.main.async {
+                            self.giftImage.image = image
+                        }
+                        print("EVERYTHING IS GOOD ON THIS SIDE")
+                        
+                    } else {
+                        print("Image file is corrupted")
+                    }
+                } else {
+                    print("No response from server")
+                }
+            }
+        }
+        
+        //starting the download task
+        getImageFromUrl.resume()
+        
     }
     
     @IBAction func closePopUp(_ sender: Any) {
